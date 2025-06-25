cf0665f8a583c78439ff94f9f123462eba290710
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2025-06-25 09:57:03 +0200

    second lesson

--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     price = models.ImageField()
+
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    reservation_time = models.DateField(auto_now=True)
+    comments = models.CharField(max_length=1000)
